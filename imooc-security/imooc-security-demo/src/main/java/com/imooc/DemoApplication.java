318dca040a2022da8efde1da5324454575db7af9
q1054261752 <1054261752@qq.com>
2017-10-19 18:43:39 +0800

    2-1 开发环境安装 2-2 代码结构介绍 2-3 Hello Spring Security

---
+++ imooc-security/imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zkr on 2017/10/19.
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello spring security";
+    }
+}
